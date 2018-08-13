de6c1bc69d90a114654656f723266d05662c43bf
John <jhawley318@gmail.com>
2018-08-13 09:36:05 -0500

    created basic turret and particle

---
+++ Assets/Turret.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	[SerializeField] ParticleSystem gun;
+	bool fire = false;
+
+	PlayerController player;
+
+	void Start()
+	{
+		player = FindObjectOfType<PlayerController>();
+
+	}
+	
+	void Update()
+	{
+		LookAtPlayer();
+
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			fire = !fire;
+			FireGun(fire);
+		}
+	}
+
+	void FireGun(bool f)
+	{
+		var emission = gun.emission;
+		emission.enabled = f;
+	}
+
+	void LookAtPlayer()
+	{
+		transform.LookAt(player.transform.position);
+	}
+}
